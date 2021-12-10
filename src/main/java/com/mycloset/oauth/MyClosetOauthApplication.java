50e1c427e43afc0ff8d19d4f8b41f7cddde02994
namookk <papakang22@naver.com>
2021-12-10 15:15:09 +0900

    프로젝트 분리작업

---
+++ src/main/java/com/mycloset/oauth/MyClosetOauthApplication.java
@@ -0,0 +1,13 @@
+package com.mycloset.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClosetOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyClosetOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycloset/oauth/MyClosetOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycloset.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyClosetOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
